4835221390341cc6c6432a47b13296450a9071dd
Miguel Coelho <miguel_coelho_1@hotmail.com>
2017-05-03 19:52:38 +0100

    Google Map
    Adição da api do google a quando da criação dos monumentos

--- ThomarWeb/config/routes.rb
+++ ThomarWeb/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/index'
   devise_for :users, controllers: { sessions: 'users/sessions'}
   resources :categories
   resources :pois
@@ -8,5 +11,5 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'monuments#index'
+  root 'home#index'
 end
